a8c88a03572346c3c68bcb3828faf3174311916e
unknown <albertocavero1771@gmail.com>
2024-02-06 13:51:55 -0600

    Primeros cambios al proyecto

---
+++ src/js/script1.js
@@ -0,0 +1,16 @@
+alert("hola");
+console.log("hola");
+
+const location1         =          "New York,"         +         "NY";
+const location2="New York,"         +         "NY";
+const location3=   "New York,"+         "NY";
+
+console.log(location1);
+console.log(location2);
+console.log(location3);
+
+let nombre = prompt("introduce tu nombre");
+console.log("tu nombre es:", nombre);
+
+
+
