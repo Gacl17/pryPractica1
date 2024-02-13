2947e45eb15fbeebda089c3df32e147028eda175
unknown <albertocavero1771@gmail.com>
2024-02-13 13:13:00 -0600

    segundo comit

---
+++ src/js/ejemplos.js
@@ -0,0 +1,1 @@
+//console.log(`francesa:${nameUser} mas texto ${}`);
--- src/js/script1.js
+++ src/js/script1.js
@@ -1,4 +1,4 @@
-alert("hola");
+/*alert("hola");
 console.log("hola");
 
 const location1         =          "New York,"         +         "NY";
@@ -9,8 +9,48 @@ console.log(location1);
 console.log(location2);
 console.log(location3);
 
+let valor1 = 45;
+let valor2 = "gael";
+
+let edad = 45;
+console.log(edad);
+
+let apellido = "cavero";
+console.log(cavero);
+
+function varTest(){
+    var x = 31; // var tiene un alcance mayor
+    if (true){
+        var x = 56;
+        console.log(x);
+    }
+    console.log(x);
+}
+varTest();
+
+console.log(x);
+let x = 34; //var permite que aunque la funcion lo mande a llamar y no este ahi lo busca pero con let marca error por eso var ya no se usa
 let nombre = prompt("introduce tu nombre");
-console.log("tu nombre es:", nombre);
+console.log("tu nombre es:", nombre);*/
 
+/*"use strict";//esto forsa la declaracion de variables
+x = 45;
+console.log(x);*/
 
+//var tiene ambito de funcion
+function ejemploAmbitoVar(){
+    if (true){
+        var x= 23;
+    }
+    console.log(x);
+}
+ejemploAmbitoVar();
+
+function ejemploAmbitoVar(){
+    if (true){
+        let x= 23;
+    }
+    console.log(x);
+}
+ejemploAmbitoVar();
 
